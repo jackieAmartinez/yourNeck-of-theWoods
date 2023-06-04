53092db3cf5ea29197d0f6052040d69e70b57c9d
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-04 14:20:04 -0400

    current forecast api success

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let apiKey = "d79e7c1302e1954e9746b6da76d3cba5";
+// let apiKey = "96d4e6e6d5c445691b5625adf211f214";
 let searchForm = $(`#searchForm`);
 let newSearch = $(`#searchInput`).val();
 let searchBtn = $(`#searchBtn`);
@@ -18,9 +18,9 @@ searchBtn.on(`click`, function (event) {
     getForecast(citySearch);
     predictForecast(citySearch);
     saveSearch(citySearch);
+    writePastSearches();
     // reset input field
     $(`#searchInput`).val("");
-    // hide the silly starter town info & show field for requested forecast
     temporary.addClass("hide");
     daysBlock.removeClass("hide");
 }
@@ -41,80 +41,57 @@ $('#recentList').on('click', function (event) {
 function getForecast(citySearch) {
     if (citySearch !== "") {
         resetForecast();
-        let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + apiKey;
-        fetch(weatherUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json()
-                        .then(function (data) {
-                            // show current forecast
-                            $(`#currentTitle`).text(data.name);
-                            $(`#currentIcon`).append(`<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" class="icon" alt="${data.weather[0].description}" />`);
-                            $(`#currentDate`).text(dayjs(data.dt * 1000).format("MM/DD/YYYY"));
-                            $(`#currentTemperature`).text("Temperature: " + data.main.temp.toFixed(0) + " °F");
-                            $(`#currentWindSpeed`).text("Wind Speed: " + data.wind.speed.toFixed(0) + " MPH");
-                            $(`#currentHumidity`).text("Humidity: " + data.main.humidity.toFixed(0) + " %");
-                        })
-                } else {
-                    alert("There seems to be an issue finding your forecast. Please try entering the city again.")
-                }
-            })
-    } else {
-        alert("You need to search a city first to get your forecast.")
-    }
-};
+        let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + "96d4e6e6d5c445691b5625adf211f214";
+        fetch(weatherUrl).then(function (response) {
+            if (response.ok) {
+              response.json().then(function (data) {
+                // show current forecast
+                $(`#currentTitle`).text(data.name);
+                $(`#currentIcon`).append(
+                  `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" class="icon" alt="${data.weather[0].description}" />`
+                );
+                $(`#currentDate`).text(dayjs(data.dt * 1000).format("MM/DD/YYYY"));
+                $(`#currentTemperature`).text(
+                  "Temperature: " + data.main.temp.toFixed(0) + " °F"
+                );
+                $(`#currentWindSpeed`).text(
+                  "Wind Speed: " + data.wind.speed.toFixed(0) + " MPH"
+                );
+                $(`#currentHumidity`).text(
+                  "Humidity: " + data.main.humidity.toFixed(0) + " %"
+                );
+              });
+            }
+          });
+        }
+      }
+        
 
 // function to fetch data to get and show 5 day future forecast
-function predictForecast(citySearch) {
-    if (citySearch !== "") {
-        resetForecast();
-        let futureUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&appid=" + apiKey;
-        fetch(futureUrl)
-            .then(function (response) {
-                if (response.ok) {
-                    response.json()
-                        .then(function (data) {
-                            // for loop to repeat through 5 day cycle of the following day cards
-                            for (i = 5; data.list.length; i += 8) {
-                                // creating elements for eah day in forecast & add classes
-                                let createDay = document.createElement("section");
-                                $(createDay).addClass("card days");
-                                let createForecastBasic = document.createElement("div");
-                                $(createForecastBasic).addClass("forecastBasic")
-                                let createIcon = document.createElement("div");
-                                $(createIcon).addClass("icon");
-                                let createDate = document.createElement("div");
-                                $(createDate).addClass("date");
-                                let createForecastDetail = document.createElement("div");
-                                $(createForecastDetail).addClass("forecastDetail");
-                                let createTemperature = document.createElement("div");
-                                $(createTemperature).addClass("stat");
-                                let createWindSpeed = document.createElement("div");
-                                $(createWindSpeed).addClass("stat");
-                                let createHumidity = document.createElement("div");
-                                $(createHumidity).addClass("stat");
-                                // combining the day elements through appending for each day of forecast to create a single forecast unit
-                                daysBlock.append(createDay);
-                                createDay.append(createForecastBasic);
-                                createForecastBasic.append(createIcon);
-                                createForecastBasic.append(createDate);
-                                createDay.append(createForecastDetail);
-                                createForecastDetail.append(createTemperature);
-                                createForecastDetail.append(createWindSpeed);
-                                createForecastDetail.append(createHumidity);
-                                // input the actual displayed information/data into the day's various elements
-                                $(createIcon).append(`<img src="https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png" class="icon" alt="${data.list[i].weather[0].description}" />`);
-                                $(createDate).text(dayjs(data.list[i].dt * 1000).format("MM/DD/YYYY"));
-                                $(createTemperature).text(`Temperature: ` + data.list[i].main.temp.toFixed(0) + ` °F`);
-                                $(createWindSpeed).text(`Wind Speed: ` + data.list[i].wind.speed.toFixed(0) + ` MPH`);
-                                $(createHumidity).text(`Humidity: ` + data.list[i].main.humidity.toFixed(0) + ` %`);
-                            }
-                        })
-                } else {
-                    alert("There seems to be an issue finding the future forecast. Please try again later.")
-                }
-            })
-    }
+function getForecast(citySearch) {
+  if (citySearch !== "") {
+      resetForecast();
+      let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=" + "96d4e6e6d5c445691b5625adf211f214";
+      fetch(weatherUrl)
+          .then(function (response) {
+              if (response.ok) {
+                  response.json()
+                      .then(function (data) {
+                          // show current forecast
+                          $(`#currentTitle`).text(data.name);
+                          $(`#currentIcon`).append(`<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" class="icon" alt="${data.weather[0].description}" />`);
+                          $(`#currentDate`).text(dayjs(data.dt * 1000).format("MM/DD/YYYY"));
+                          $(`#currentTemperature`).text("Temperature: " + data.main.temp.toFixed(0) + " °F");
+                          $(`#currentWindSpeed`).text("Wind Speed: " + data.wind.speed.toFixed(0) + " MPH");
+                          $(`#currentHumidity`).text("Humidity: " + data.main.humidity.toFixed(0) + " %");
+                      })
+              } else {
+                  alert("There seems to be an issue finding your forecast. Please try entering the city again.")
+              }
+          })
+  } else {
+      alert("You need to search a city first to get your forecast.")
+  }
 };
 
 // function to reset weather display
